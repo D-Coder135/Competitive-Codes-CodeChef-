5c4e143c35bbeb20a05b4f53192909beed94c619
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-09-14 21:36:49 +0530

    Add the code to find the correct hacked password

--- src/PassHack.java
+++ src/PassHack.java
@@ -9,5 +9,27 @@ import java.util.Scanner;
 public class PassHack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        for (int numberOfInputs = 0; numberOfInputs < testCases; numberOfInputs++) {
+            String revPassword = "";
+            String password = "";
+            int wrongPassword = scanner.nextInt();
+            int conditionVariable = wrongPassword;
+            while (conditionVariable != 0) {
+                int eachDigit = conditionVariable % 10;
+                conditionVariable = conditionVariable / 10;
+                if (eachDigit <= 1) {
+                    break;
+                } else {
+                    eachDigit = eachDigit - 2;
+                    revPassword = revPassword.concat(String.valueOf(eachDigit));
+                }
+            }
+            for (int reverseIndex = revPassword.length() - 1; reverseIndex >= 0; reverseIndex--) {
+                password = password.concat(String.valueOf(revPassword.charAt(reverseIndex)));
+            }
+            int correctPassword = Integer.parseInt(password);
+            System.out.println(correctPassword);
+        }
     }
 }
